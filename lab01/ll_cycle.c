f88626e96281ae00b450bbced39a30c9214383bb
suxiaopeng <1211400223@qq.com>
2022-08-13 21:26:40 +0800

    complete lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortois = head;
+    node *hare = head;
+    
+    while (hare) {
+	if (!hare->next){
+	        return 0;
+	}	
+	hare = hare -> next -> next;
+	tortois = tortois -> next;
+
+    	if (hare == tortois) {
+    		return 1;
+    	}
+    }
+
+
     return 0;
 }
